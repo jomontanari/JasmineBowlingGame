953878de10d378412364f19abf3e3d42b13e697a
Jo Cranford <jo.cranford@gmail.com>
2011-04-11 14:46:43 +0100

    Updated powerpoint

--- spec/frame.spec.js
+++ spec/frame.spec.js
@@ -1,59 +1,67 @@
 describe("the frame", function() {
 
-    it("should be complete if two rolls have been made", function() {
+    describe("Knowing when it is complete", function() {
 
-        var frame = caek.frame();
-        frame.addRoll(3);
-        frame.addRoll(5);
+        it("should be complete if two rolls have been made", function() {
 
-        expect(frame.isComplete()).toBeTruthy();
+            var frame = caek.frame();
+            frame.addRoll(3);
+            frame.addRoll(5);
 
-    });
+            expect(frame.isComplete()).toBeTruthy();
 
-    it("should be complete if all the skittles are knocked down", function() {
+        });
 
-        var frame = caek.frame();
-        frame.addRoll(10);
+        it("should be complete if all the skittles are knocked down", function() {
 
-        expect(frame.isComplete()).toBeTruthy();
-    });
+            var frame = caek.frame();
+            frame.addRoll(10);
 
-    it("should be incomplete if first roll does not knock down all skittles", function() {
+            expect(frame.isComplete()).toBeTruthy();
+        });
 
-        var frame = caek.frame();
-        frame.addRoll(3);
+        it("should be incomplete if first roll does not knock down all skittles", function() {
 
-        expect(frame.isComplete()).toBeFalsy();
-    });
+            var frame = caek.frame();
+            frame.addRoll(3);
 
-    it("should correctly calculate a score as the sum of two rolls", function() {
-
-        var frame = caek.frame();
-        frame.addRoll(3);
-        frame.addRoll(2);
-
-        expect(frame.score()).toBe(5);
+            expect(frame.isComplete()).toBeFalsy();
+        });
 
     });
 
-    it("should correctly calculate the score of a spare including the bonus", function() {
+    describe("Score calculation", function() {
 
-        var frame = caek.frame();
-        frame.addRoll(9);
-        frame.addRoll(1);
-        frame.setSpareBonus(4);
+        it("should correctly calculate a score as the sum of two rolls", function() {
+
+            var frame = caek.frame();
+            frame.addRoll(3);
+            frame.addRoll(2);
+
+            expect(frame.score()).toBe(5);
+
+        });
+
+        it("should correctly calculate the score of a spare including the bonus", function() {
+
+            var frame = caek.frame();
+            frame.addRoll(9);
+            frame.addRoll(1);
+            frame.setSpareBonus(4);
 
 
-        expect(frame.score()).toBe(14);
-    });
+            expect(frame.score()).toBe(14);
+        });
 
-    it("should correctly calculate the score of a strike including the bonus", function() {
+        it("should correctly calculate the score of a strike including the bonus", function() {
 
-        var frame = caek.frame();
-        frame.addRoll(10);
-        frame.setStrikeBonus(8);
+            var frame = caek.frame();
+            frame.addRoll(10);
+            frame.setStrikeBonus(8);
 
-        expect(frame.score()).toBe(18);
+            expect(frame.score()).toBe(18);
+        });
+
     });
 
 });
